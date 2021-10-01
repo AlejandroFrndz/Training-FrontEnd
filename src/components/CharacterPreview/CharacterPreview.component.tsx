a43380e64fe98c488a215ec742a1079f255e81fb
Alejandro Fernandez <alejandro.f@strivelabs.io>
2021-10-01 19:25:49 +0200

    styles: Added visual styling to the characters preview card and made the list responsive update: Added status display to characters preview

--- src/components/CharacterPreview/CharacterPreview.component.tsx
+++ src/components/CharacterPreview/CharacterPreview.component.tsx
@@ -1,15 +1,37 @@
+import styles from "./styles.module.css";
+
 interface Props {
     name: string,
-    src: string
+    src: string,
+    status: string
 }
 
 const CharacterPreview : React.FC<Props> = (props) => {
-    const { name, src } = props;
+    const { name, src, status } = props;
 
     return(
-        <div className="charPreview">
-            <h1>{name}</h1>
-            <img src={src} alt={`${name}'s face`} />
+        <div className={`charPreview container`}>
+            <div className="row">
+                <div className="col-12">
+                    <h2 className="text-center">{name}</h2>
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-12">
+                    <img className={styles.flexImg} src={src} alt={`${name}'s face`} />
+                </div>
+            </div>
+            <div className={`row ${styles.mt}`}>
+                <div className="col">
+                    <p>Status:</p>
+                </div>
+                <div className="col" >
+                    <button id={status === "Alive" ? styles.aliveLight : styles.deadLight} type="button" />
+                </div>
+                <div className="col" >
+                    <p>{status}</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/CharactersList/CharactersList.component.tsx
+++ src/components/CharactersList/CharactersList.component.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import CharacterData from "../../types/character.type";
 import CharacterPreview from "../CharacterPreview/CharacterPreview.component";
+import styles from "./style.module.css";
 
 interface Props {
     characters: CharacterData[],
@@ -11,10 +12,19 @@ const CharactersList: React.FC<Props> = (props) => {
     const { characters, onGoBack } = props;
 
     return(
-        <>
-            {characters.length > 0 ? characters.map((character) => <Link to={`/characters/${character.id}`} key={character.id}><CharacterPreview name={character.name} src={character.image} /></Link>) : "No characters in record"}
-            <button onClick={onGoBack}>Go Back</button>
-        </>
+        <div className="container">
+            <div className="row justify-content-center">
+                {characters.length > 0 ? characters.map((character,index) => {
+                    return(
+                        <div key={character.id} className={`col-4 me-2 mb-2 pb-3 ${styles.charWrapper} ${styles.overrideWidth}`}>
+                            <Link to={`/characters/${character.id}`} className="text-decoration-none text-reset">
+                                <CharacterPreview name={character.name} src={character.image} status={character.status}/>
+                            </Link>
+                        </div>
+                    );
+                }) : "No characters in record"}
+            </div>
+        </div>
     )
 }
 
