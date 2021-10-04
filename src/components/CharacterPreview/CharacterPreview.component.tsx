c9f00d5f17c07f72333300e769577ad6b5beb276
Alejandro Fernandez <alejandro.f@strivelabs.io>
2021-10-04 08:55:46 +0200

    styles: Made character preview name stay inside div, breaking it if needed

--- src/components/CharacterPreview/CharacterPreview.component.tsx
+++ src/components/CharacterPreview/CharacterPreview.component.tsx
@@ -8,28 +8,32 @@ interface Props {
 
 const CharacterPreview : React.FC<Props> = (props) => {
     const { name, src, status } = props;
+    let wordBreak = false;
+    const splitName = name.split(" ");
+
+    for(let str of splitName){
+        if(str.length >= 12){
+            wordBreak = true;
+            break;
+        }
+    }
+
 
     return(
         <div className={`charPreview container`}>
             <div className="row">
                 <div className="col-12">
-                    <h2 className="text-center">{name}</h2>
+                    <h2 className={`text-center ${wordBreak ? "text-break" : ""}`}>{name}</h2>
                 </div>
             </div>
-            <div className="row justify-content-center">
+            <div className="row">
                 <div className="col-12">
-                    <img className={styles.flexImg} src={src} alt={`${name}'s face`} />
+                    <img className={`${styles.flexImg} text-center`} src={src} alt={`${name}'s face`} />
                 </div>
             </div>
             <div className={`row ${styles.mt}`}>
-                <div className="col">
-                    <p>Status:</p>
-                </div>
-                <div className="col" >
+                <div className="col-12 text-center" >
                     <button id={status === "Alive" ? styles.aliveLight : styles.deadLight} type="button" />
-                </div>
-                <div className="col" >
-                    <p>{status}</p>
                 </div>
             </div>
         </div>
